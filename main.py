39f26c4a10815676519e9f5cc996c2fd624e3b0b
Ayush Gandhi <ayushgandhi904@gmail.com>
2024-01-27 22:12:16 +0530

    Model Trainer step completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mloops_project import logger
 from mloops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mloops_project.pipeline.stage_02_data_validation import DataValidationPipeline
 from mloops_project.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from mloops_project.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -31,6 +32,17 @@ try:
     logger.info(f"---stage {STAGE_NAME}---")
     data_transformation = DataTransformationPipeline()
     data_transformation.main()
+    logger.info(f"---stage {STAGE_NAME} completed---")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f"---stage {STAGE_NAME}---")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
     logger.info(f"---stage {STAGE_NAME} completed---")
 except Exception as e:
     logger.exception(e)
