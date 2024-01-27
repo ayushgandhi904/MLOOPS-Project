0e495b24d2465637aaa7bec2b226c8b4d7bfd45c
Ayush Gandhi <ayushgandhi904@gmail.com>
2024-01-27 21:45:59 +0530

    Data Transformation step completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mloops_project import logger
 from mloops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mloops_project.pipeline.stage_02_data_validation import DataValidationPipeline
+from mloops_project.pipeline.stage_03_data_transformation import DataTransformationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -19,6 +20,17 @@ try:
     logger.info(f"---stage {STAGE_NAME}---")
     data_validation = DataValidationPipeline()
     data_validation.main()
+    logger.info(f"---stage {STAGE_NAME} completed---")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f"---stage {STAGE_NAME}---")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
     logger.info(f"---stage {STAGE_NAME} completed---")
 except Exception as e:
     logger.exception(e)
